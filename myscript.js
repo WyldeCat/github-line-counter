0ec3dd170fb2d9c31e8754253a0dc2cdf510fa65
WyldeCat <skan1543@gmail.com>
2017-05-15 01:06:47 +0900

    Implement document loader function

--- myscript.js
+++ myscript.js
@@ -1,1 +1,26 @@
-alert("hello, world!");
+function loadDocument(URL) {
+  var xhr = new XMLHttpRequest();
+  try {
+    var docObj = { };
+    var check = 0;
+    xhr.open("GET", URL, false);
+    xhr.onload = function() {
+      check = 1;
+      docObj.doc = document;
+    }
+    xhr.send();
+    //while(check == 0);
+    return docObj
+  }
+  catch(e) {
+    window.alert("loading document fail..");
+  }
+}
+
+function calculateURL(body, isCurr) {
+  /* TODO */
+  return 1;
+}
+
+var test_doc = loadDocument("https://github.com");
+console.log(test_doc.doc);
